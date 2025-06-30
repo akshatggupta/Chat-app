0b49522d1d5d0d48de50f648cbce787d24b05379
akshatggupta <guptakshat333@gmail.com>
2025-06-30 23:20:51 +0400

    rewrite chat server as asynchronous

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,20 +1,40 @@
 import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
+class ChatConsumer(AsyncWebsocketConsumer):
 
-class ChatConsumer(WebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
 
-    def connect(self):
-        self.accept()
+        # Connect user to group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
 
-   
-   
-    def disconnect(self,close_code):
-        pass
-        
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    def receive(self,text_data):
+    # Receive message from WebSocket
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        self.send(text_data=json.dumps({"message":message}))
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event["message"]
+        await self.send(text_data=json.dumps({"message": message}))
--- mysite/asgi.py
+++ mysite/asgi.py
@@ -13,6 +13,8 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django_asgi_app = get_asgi_application()
 
